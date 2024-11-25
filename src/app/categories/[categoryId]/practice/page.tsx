7b964680c98fec52afbf23eeb4da5d2fda684adb
Eric Nichols <ebn646@hotmail.com>
2024-11-24 19:36:27 -0500

    feat: shows message if user has no cards in a category

--- src/app/categories/[categoryId]/practice/page.tsx
+++ src/app/categories/[categoryId]/practice/page.tsx
@@ -4,30 +4,53 @@ import { sampleCards, sampleCategories } from "@/data/sampleData";
 import { QueryClient } from "@tanstack/react-query";
 import { getCardsByCategoryId, getCategoryName } from "@/actions/actions";
 import Breadcrumbs from "@/components/Breadcrumbs";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 // prefetch the category and cards with tanstack query and categoryId
-  // Create dynamic breadcrumbs using the category name
+// Create dynamic breadcrumbs using the category name
 
-const PracticePage = async ({params}: {params: {categoryId: string}}) => {
+const PracticePage = async ({ params }: { params: { categoryId: string } }) => {
   const queryClient = new QueryClient();
 
   // Prefetch and immediately get the data
   await queryClient.prefetchQuery({
-    queryKey: ['categoryName', params.categoryId],
+    queryKey: ["categoryName", params.categoryId],
     queryFn: () => getCategoryName(params.categoryId),
   });
   const categoryName = await getCategoryName(params.categoryId);
-  
+
   await queryClient.prefetchQuery({
-    queryKey: ['cards', params.categoryId],
+    queryKey: ["cards", params.categoryId],
     queryFn: () => getCardsByCategoryId(params.categoryId),
   });
   const cards = await getCardsByCategoryId(params.categoryId);
 
+  console.log("cards", cards);
+
+  // if there are no cards, redirect to the create page
+
   const breadcrumbItems = [
     { label: "Home", href: "/" },
     { label: "Categories", href: "/categories" },
-    { label: "Practice" } // Current page, no href
+    { label: "Practice" }, // Current page, no href
   ];
+
+  if (cards.length === 0) {
+    return (
+      <div className="container max-w-3xl p-4 ">
+        <Breadcrumbs items={breadcrumbItems} />
+      <div className="flex w-full items-center justify-center font-semibold text-2xl mb-4 ">
+        <h1>{categoryName}</h1>
+      </div>
+        <div className="flex w-full items-center justify-center font-semibold text-2xl mb-4 ">
+          There are no cards in this category. Please create some cards.
+        </div>
+        <Button>
+          <Link href={`/categories/${params.categoryId}/manage`}>Create Cards</Link>
+        </Button>
+      </div>
+    );
+  }
 
   return (
     <div className="container max-w-3xl p-4 ">
--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,6 +1,13 @@
 import { SignIn } from '@clerk/nextjs'
+import { auth } from '@clerk/nextjs/server';
 import Image from 'next/image'
+import { redirect } from 'next/navigation';
 export default function Page() {
+  // check if user is authenticated
+  const { userId } = auth();
+  if (userId) {
+    redirect("/categories");
+  }
   return (
     <div className="flex items-center justify-center h-full w-full py-12">
       <div className="flex-1 flex items-center justify-center h-full">
